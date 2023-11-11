baea081f7b8feb36995d4e094b2ed4a76b225c4e
jubayed004 <mohammadjubayed.islam97@gmail.com>
2023-11-11 18:30:16 +0600

    feat(onboarding screen): incompleted

---
+++ lib/features/authentication/controllers_onboarding/onboarding_controller.dart
@@ -0,0 +1,23 @@
+import 'package:get/get.dart';
+
+class OnBoardingController extends GetxController{
+  static OnBoardingController get instance => Get.find();
+
+  /// Variable
+
+  /// Update Current index when page Scroll
+  void updatePageIndicator (){}
+
+  ///Jump to the specific dot selected page
+  void dotNavigationClick (){}
+
+  /// Update Current Index & Jump to Next page
+
+  void nextPage (){}
+
+  ///Update Current Index & Jump to the last Page
+  void skipPage(){}
+
+
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:e_commerce_app/utils/theme/theme.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'app.dart';
+
+void main() {
+/*  DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => App(), // Wrap your app
+  );*/
+runApp(const App());
+
+}
